266e8e2f13ab01b16ba7108051e53d5a901380b3
AkshitaKumari156 <ashiakshitaaa@gmail.com>
2025-06-27 20:19:45 +0530

    Depression

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,25 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import Home from './depression/pages/Home'
 import DepressionTest from './depression/pages/Depressiontest'
-
+import ReliefPage from './depression/pages/ReliefPage'
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home/>} />
-        <Route
-          path="/"
-          element={<Home/>}
-        />
+        <Route path="/home-Depression" element={<Home/>} />
         <Route
           path="/test"
           element={<DepressionTest/>}
         />
          <Route
           path="/relief-plan"
-          element={<DepressionTest/>}
+          element={
+            <ReliefPage/>
+          }
         />
       </Routes>
     </Router>
